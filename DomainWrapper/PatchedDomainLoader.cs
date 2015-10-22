a3c64f5808e27a1207c7e02c65ad9a14a26be791
JacobKemple <JacobKemple@outlook.com>
2015-10-22 04:48:55 -0700

    First upload.

---
+++ DomainWrapper/PatchedDomainLoader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.InteropServices;
+using RGiesecke.DllExport;
+
+namespace DomainWrapper
+{
+    /// <summary>
+    ///     The exported methods to call to host our domain.
+    /// </summary>
+    [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
+    public static class PatchedDomainLoader
+    {
+        internal static string ApplicationToHostName = "WhiteVex";
+
+        [MarshalAs(UnmanagedType.LPWStr)] internal static string ApplicationToHostDirectory = @"C:\Users\Jacob Kemple\Documents\Visual Studio 2015\Projects\WhiteVex\WhiteVex\bin\Debug\";
+
+
+        [DllExport]
+        [STAThread]
+        public static void HostDomain()
+        {
+#if LAUNCH_MDA
+            System.Diagnostics.Debugger.Launch();
+#endif
+            ApplicationToHostDirectory = Path.GetDirectoryName(ApplicationToHostDirectory);
+            Trace.Assert(Directory.Exists(ApplicationToHostDirectory));
+            Trace.Listeners.Add(
+                new TextWriterTraceListener(Path.Combine(ApplicationToHostDirectory, @"Logs\",
+                    ApplicationToHostName + ".PatchedDomainLoader.log")));
+
+            try
+            {
+                using (var host = new PathedDomainHost(ApplicationToHostName, ApplicationToHostDirectory))
+                {
+                    host.Execute();
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+            }
+        }
+
+        [DllExport("LoadDomainSettings", CallingConvention.Cdecl)]
+        public static void LoadDomainHostSettings(string loadDirectory, string applicationName)
+        {
+            ApplicationToHostDirectory = loadDirectory;
+            ApplicationToHostName = applicationName;
+        }
+    }
+}
---
+++ DomainWrapper/PathedDomainHost.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Runtime.ConstrainedExecution;
+
+namespace DomainWrapper
+{
+    /// <summary>
+    ///     The class representing the object to host.
+    /// </summary>
+    public class PathedDomainHost : CriticalFinalizerObject, IDisposable
+    {
+        private readonly string _dllPath;
+        private AppDomain _hostDomain;
+
+        public PathedDomainHost(string name, string path)
+        {
+            var setupInfo = new AppDomainSetup
+            {
+                ApplicationBase = path,
+                PrivateBinPath = path
+            };
+
+            _dllPath = Path.Combine(path, Path.ChangeExtension(name, "exe"));
+            _hostDomain = AppDomain.CreateDomain(name, AppDomain.CurrentDomain.Evidence, setupInfo);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~PathedDomainHost()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_hostDomain != null)
+            {
+                AppDomain.Unload(_hostDomain);
+                _hostDomain = null;
+            }
+        }
+
+        public void Execute()
+        {
+            _hostDomain.ExecuteAssembly(_dllPath);
+        }
+
+        public enum ClickToMove
+        {
+            Push = 0x1C,
+            X = 0x84,
+            Y = X + 0X4,
+            Z = +0X8,
+            Guid = 0x20,
+            Distance = 0xc
+        }
+
+  
+    }
+}
